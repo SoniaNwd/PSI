7af25813b06bab5288c6bca9a1922807ad320e71
SoniaNwd <sonia.niewiadomska@gmail.com>
2026-03-03 16:36:35 +0100

    Add files via upload

---
+++ zajecia1/z1.2PSI.R
@@ -0,0 +1,14 @@
+ocena_kredytowa = function(dochod, zadluzenie) {
+  procent = zadluzenie/dochod
+  
+  if (procent < 0.3) {
+    return("KREDYT PRZYZNANY")
+  } else if (procent >= 0.3 & procent <= 0.5){
+    return("WYMAGA WERYFIKACJI")
+  }else {
+    return("KREDYT ODRZUCONY")
+  }
+}
+ocena_kredytowa(10000, 2000)
+ocena_kredytowa(10000, 4000)
+ocena_kredytowa(10000, 6000)
